87aa3da7ea30340aa61ccce9a4e4142d3136b57c
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-07-07 14:32:14 +0200

    Styling FancyTab

--- source/darefl/mainwindow/fancytab.cpp
+++ source/darefl/mainwindow/fancytab.cpp
@@ -12,12 +12,28 @@
 #include <QMouseEvent>
 #include <QPainter>
 #include <darefl/mainwindow/fancytab.h>
+#include <mvvm/widgets/widgetutils.h>
+
+namespace
+{
+QColor defaultColor()
+{
+    static QWidget tmpWidget;
+    return tmpWidget.palette().color(QPalette::Window);
+}
+} // namespace
 
 FancyTab::FancyTab(const QString& title, QWidget* parent)
     : QWidget(parent), m_label(new QLabel(title))
 {
+    ModelView::Utils::ScaleLabelFont(m_label, 1.25);
+    setFixedHeight(ModelView::Utils::HeightOfLetterM() * 2.5);
+
     auto layout = new QHBoxLayout(this);
+    layout->setContentsMargins(0, 0, 0, 0);
+
     layout->addWidget(m_label, 0, Qt::AlignCenter);
+    setMouseTracking(true);
 }
 
 void FancyTab::setSelected(bool value)
@@ -30,12 +46,31 @@ void FancyTab::paintEvent(QPaintEvent*)
 {
     QPainter painter(this);
 
-    if (m_is_selected)
-        painter.drawLine(0, size().height()-2, size().width(), size().height()-2);
+    if (m_widgetColor.isValid())
+        painter.fillRect(0, 0, size().width(), size().height(), m_widgetColor);
+
+    if (m_is_selected && isEnabled())
+        painter.fillRect(
+            QRectF(QPointF(0, size().height() - 2), QPointF(size().width(), size().height())),
+            QColor("#1563c7"));
 }
 
 void FancyTab::mousePressEvent(QMouseEvent* event)
 {
     if (isEnabled() && event->button() == Qt::LeftButton)
         clicked();
+}
+
+void FancyTab::enterEvent(QEvent*)
+{
+    if (isEnabled())
+        m_widgetColor = QColor(Qt::lightGray);
+    update();
+}
+
+void FancyTab::leaveEvent(QEvent*)
+{
+    if (isEnabled())
+        m_widgetColor = defaultColor();
+    update();
 }
--- source/darefl/mainwindow/fancytab.h
+++ source/darefl/mainwindow/fancytab.h
@@ -11,6 +11,7 @@
 #define DAREFL_MAINWINDOW_FANCYTAB_H
 
 #include <QWidget>
+#include <QColor>
 
 class QLabel;
 class QString;
@@ -30,10 +31,13 @@ signals:
 protected:
     void paintEvent(QPaintEvent*) override;
     void mousePressEvent(QMouseEvent *event) override;
+    void enterEvent(QEvent *) override;
+    void leaveEvent(QEvent *) override;
 
 private:
     QLabel* m_label{nullptr};
     bool m_is_selected{false};
+    QColor m_widgetColor;
 };
 
 #endif // DAREFL_MAINWINDOW_FANCYTAB_H
