716126ff832b50e5464efd045f746a38a55f3d0e
AlexanderSchober <alex.schober@mac.com>
2020-07-23 08:04:02 +0300

    Comment out the new simulation widget

--- source/darefl/mainwindow/mainwindow.cpp
+++ source/darefl/mainwindow/mainwindow.cpp
@@ -70,17 +70,20 @@ void MainWindow::init_components()
     m_welcomeView = new WelcomeView(m_models.get());
     m_importDataView = new ImportDataView(m_models.get());
     m_reflWindow = new ReflDockWindow(m_models.get());
-    m_sim_widget = new SimulationWidget();
     m_settingsView = new SettingsView(m_models.get());
-
     m_barWidget = new MainBarWidget;
 
-    m_sim_widget->setModels(models.get());
+    // __________________________________________
+    // THE NEW SIMULATION WIDGET UNCOMMENT TO USE
+    // m_sim_widget = new SimulationWidget();
+    // m_sim_widget->setModels(m_models.get());
 
     m_barWidget->addWidget(m_welcomeView, "Project");
     m_barWidget->addWidget(m_importDataView, "Data");
     m_barWidget->addWidget(m_reflWindow, "Simulation");
-    m_barWidget->addWidget(m_sim_widget, "Fitting");
+    // __________________________________________
+    // THE NEW SIMULATION WIDGET UNCOMMENT TO USE
+    m_barWidget->addWidget(new QWidget, "Fitting");//m_sim_widget, "Fitting");
     m_barWidget->addWidget(new QWidget, "Export", false);
     m_barWidget->addWidget(m_settingsView, "Settings");
     m_barWidget->setCurrentIndex(0);
