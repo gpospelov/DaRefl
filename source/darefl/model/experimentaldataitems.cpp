d9e775c00762cd59591f3c218a5d4994d1aad048
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-07-28 13:24:02 +0200

    Provide custom ymax for CanvasItem

--- source/darefl/model/experimentaldataitems.cpp
+++ source/darefl/model/experimentaldataitems.cpp
@@ -9,10 +9,20 @@
 
 #include <darefl/model/item_constants.h>
 #include <darefl/model/experimentaldataitems.h>
+#include <mvvm/standarditems/axisitems.h>
 
 using namespace ModelView;
 
-CanvasItem::CanvasItem() : GraphViewportItem(::Constants::CanvasItemType) {}
+CanvasItem::CanvasItem() : GraphViewportItem(::Constants::CanvasItemType)
+{
+    yAxis()->setProperty(ViewportAxisItem::P_IS_LOG, true);
+}
+
+std::pair<double, double> CanvasItem::data_yaxis_range() const
+{
+    auto [ymin, ymax] = GraphViewportItem::data_yaxis_range();
+    return {ymin, ymax*2.0};
+}
 
 CanvasContainerItem::CanvasContainerItem() : ContainerItem(::Constants::CanvasContainerItemType) {}
 
