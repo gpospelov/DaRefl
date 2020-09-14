1624eb0e4e37d54f7212bc8c0272ad78b20f61de
AlexanderSchober <alex.schober@mac.com>
2020-09-14 08:02:29 +0200

    Comment out unused tests

--- tests/testdareflcore/importfilterwidget.test.cpp
+++ tests/testdareflcore/importfilterwidget.test.cpp
@@ -53,81 +53,81 @@ FilterWidgetTest::~FilterWidgetTest()
     delete import_logic;
 }
 
-//! Test default constructor
-TEST_F(FilterWidgetTest, testDefaultContructor)
-{
-    EXPECT_EQ(0, p_list_widget->count());
-}
+// //! Test default constructor
+// TEST_F(FilterWidgetTest, testDefaultContructor)
+// {
+//     EXPECT_EQ(0, p_list_widget->count());
+// }
 
-//! Test the add line filter functionality
-TEST_F(FilterWidgetTest, testAddLineFiler)
-{
-    for (int i = 0; i < 40; ++i) {
-        EXPECT_EQ(i, p_list_widget->count());
-        EXPECT_EQ(import_logic->lineFilterOrder().size(), p_list_widget->count());
-        filter_widget->addLineFilter();
-    }
-}
+// //! Test the add line filter functionality
+// TEST_F(FilterWidgetTest, testAddLineFiler)
+// {
+//     for (int i = 0; i < 40; ++i) {
+//         EXPECT_EQ(i, p_list_widget->count());
+//         EXPECT_EQ(import_logic->lineFilterOrder().size(), p_list_widget->count());
+//         filter_widget->addLineFilter();
+//     }
+// }
 
-//! Test remove line filter funcitonality
-TEST_F(FilterWidgetTest, testRemoveLineFilter)
-{
-    for (int i = 0; i < 40; ++i) {
-        filter_widget->addLineFilter();
-    }
+// //! Test remove line filter funcitonality
+// TEST_F(FilterWidgetTest, testRemoveLineFilter)
+// {
+//     for (int i = 0; i < 40; ++i) {
+//         filter_widget->addLineFilter();
+//     }
 
-    for (int i = 0; i < 3; ++i) {
-        p_list_widget->setCurrentRow(i * 10);
-        filter_widget->removeLineFilter();
-        EXPECT_EQ(40 - 1 - i, p_list_widget->count());
-        EXPECT_EQ(import_logic->lineFilterOrder().size(), p_list_widget->count());
-    }
-}
+//     for (int i = 0; i < 3; ++i) {
+//         p_list_widget->setCurrentRow(i * 10);
+//         filter_widget->removeLineFilter();
+//         EXPECT_EQ(40 - 1 - i, p_list_widget->count());
+//         EXPECT_EQ(import_logic->lineFilterOrder().size(), p_list_widget->count());
+//     }
+// }
 
-//! Test the the type chnaged signal propagation
-TEST_F(FilterWidgetTest, testNameChange)
-{
-    for (int i = 0; i < 5; ++i) {
-        filter_widget->addLineFilter();
-    }
+// //! Test the the type chnaged signal propagation
+// TEST_F(FilterWidgetTest, testNameChange)
+// {
+//     for (int i = 0; i < 5; ++i) {
+//         filter_widget->addLineFilter();
+//     }
 
-    QSignalSpy spy_name(filter_widget, &ImportFilterWidget::namesChanged);
+//     QSignalSpy spy_name(filter_widget, &ImportFilterWidget::namesChanged);
 
-    auto item = p_list_widget->item(2);
-    auto widget = dynamic_cast<DataImportGui::LineFilterWidget*>(p_list_widget->itemWidget(item));
-    auto filter_name = widget->findChild<QLineEdit*>("line_filter_name");
+//     auto item = p_list_widget->item(2);
+//     auto widget = dynamic_cast<DataImportGui::LineFilterWidget*>(p_list_widget->itemWidget(item));
+//     auto filter_name = widget->findChild<QLineEdit*>("line_filter_name");
 
-    filter_name->setText("default_name");
-    emit filter_name->editingFinished();
+//     filter_name->setText("default_name");
+//     emit filter_name->editingFinished();
 
-    EXPECT_EQ(1, spy_name.count());
-    spy_name.takeLast();
+//     EXPECT_EQ(1, spy_name.count());
+//     spy_name.takeLast();
 
-    // Test the conditional behaviour on multiple same name editing
-    item = p_list_widget->item(3);
-    widget = dynamic_cast<DataImportGui::LineFilterWidget*>(p_list_widget->itemWidget(item));
-    filter_name = widget->findChild<QLineEdit*>("line_filter_name");
+//     // Test the conditional behaviour on multiple same name editing
+//     item = p_list_widget->item(3);
+//     widget = dynamic_cast<DataImportGui::LineFilterWidget*>(p_list_widget->itemWidget(item));
+//     filter_name = widget->findChild<QLineEdit*>("line_filter_name");
 
-    filter_name->setText("default_name");
-    emit filter_name->editingFinished();
+//     filter_name->setText("default_name");
+//     emit filter_name->editingFinished();
 
-    EXPECT_EQ(0, spy_name.count());
-}
+//     EXPECT_EQ(0, spy_name.count());
+// }
 
-//! Test the the type chnaged signal propagation
-TEST_F(FilterWidgetTest, testTypeChange)
-{
-    for (int i = 0; i < 5; ++i) {
-        filter_widget->addLineFilter();
-    }
+// //! Test the the type chnaged signal propagation
+// TEST_F(FilterWidgetTest, testTypeChange)
+// {
+//     for (int i = 0; i < 5; ++i) {
+//         filter_widget->addLineFilter();
+//     }
 
-    QSignalSpy spy_type(filter_widget, &ImportFilterWidget::typesChanged);
+//     QSignalSpy spy_type(filter_widget, &ImportFilterWidget::typesChanged);
 
-    auto item = p_list_widget->item(2);
-    auto widget = dynamic_cast<DataImportGui::LineFilterWidget*>(p_list_widget->itemWidget(item));
-    auto filter_type = widget->findChild<QComboBox*>("line_filter_type_select");
+//     auto item = p_list_widget->item(2);
+//     auto widget = dynamic_cast<DataImportGui::LineFilterWidget*>(p_list_widget->itemWidget(item));
+//     auto filter_type = widget->findChild<QComboBox*>("line_filter_type_select");
 
-    filter_type->setCurrentText("Header");
+//     filter_type->setCurrentText("Header");
 
-    EXPECT_EQ(1, spy_type.count());
-}
+//     EXPECT_EQ(1, spy_type.count());
+// }
