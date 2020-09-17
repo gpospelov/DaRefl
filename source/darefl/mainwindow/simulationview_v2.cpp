23fb832522f8a0baa18b25a7feded611d9ce6546
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-09-17 11:47:30 +0200

    Placeholder for new SimulationView

---
+++ source/darefl/mainwindow/simulationview_v2.cpp
@@ -0,0 +1,15 @@
+// ************************************************************************** //
+//
+//  Reflectometry simulation software prototype
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include <darefl/mainwindow/simulationview_v2.h>
+
+SimulationViewV2::SimulationViewV2(ApplicationModels* models, QWidget* parent)
+    : QMainWindow(parent), m_models(models)
+{
+}
