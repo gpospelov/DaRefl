8cc62f57189196758685a8b0cca311ae86c4da90
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-18 12:54:25 +0200

    Introduce placeholders for ImportTableWidget

---
+++ source/darefl/dataloader2/importtablewidget.cpp
@@ -0,0 +1,12 @@
+// ************************************************************************** //
+//
+//  Reflectometry simulation software prototype
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include <darefl/dataloader2/importtablewidget.h>
+
+ImportTableWidget::ImportTableWidget(QWidget* parent) : QWidget(parent) {}
