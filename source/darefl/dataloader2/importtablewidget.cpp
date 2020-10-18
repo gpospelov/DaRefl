1ace3c12632c4a2d35e103236a6eae0d3622d535
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-18 13:01:55 +0200

    Add model and tableView to widget

--- source/darefl/dataloader2/importtablewidget.cpp
+++ source/darefl/dataloader2/importtablewidget.cpp
@@ -7,6 +7,15 @@
 //
 // ************************************************************************** //
 
+#include <QStandardItemModel>
+#include <QTableView>
+#include <QVBoxLayout>
 #include <darefl/dataloader2/importtablewidget.h>
 
-ImportTableWidget::ImportTableWidget(QWidget* parent) : QWidget(parent) {}
+ImportTableWidget::ImportTableWidget(QWidget* parent)
+    : QWidget(parent), m_model(new QStandardItemModel), m_tableView(new QTableView)
+{
+    auto layout = new QVBoxLayout(this);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->addWidget(m_tableView);
+}
