06f27bae9bcfece854108394b5eb0ebe7c16ca5f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-19 15:16:42 +0200

    Create place for utility rows in ImportTableModel and provide their labeling

--- source/darefl/dataloader2/importtablemodel.cpp
+++ source/darefl/dataloader2/importtablemodel.cpp
@@ -20,13 +20,16 @@ int maxColumnCount(const ImportTableModelV2::raw_data_t& data)
     return result;
 }
 
-} // namespace
+//! Names of utility row on top of the table.
+const QStringList utilityRowNames = {"Name", "Type", "Unit", "Multiplier"};
+
+}; // namespace
 
 ImportTableModelV2::ImportTableModelV2(QObject* parent) : QAbstractTableModel(parent) {}
 
 int ImportTableModelV2::rowCount(const QModelIndex&) const
 {
-    return static_cast<int>(m_rawData.size());
+    return static_cast<int>(m_rawData.size()) + utilityRowCount();
 }
 
 int ImportTableModelV2::columnCount(const QModelIndex&) const
@@ -39,12 +42,8 @@ QVariant ImportTableModelV2::data(const QModelIndex& index, int role) const
     if (!index.isValid())
         return QVariant();
 
-    if (role == Qt::DisplayRole || role == Qt::EditRole) {
-        if (index.column() <= static_cast<int>(m_rawData[index.row()].size())) {
-            auto str = m_rawData[static_cast<int>(index.row())][static_cast<int>(index.column())];
-            return QString::fromStdString(str);
-        }
-    }
+    if (role == Qt::DisplayRole || role == Qt::EditRole)
+        return dataFromIndex(index);
 
     return QVariant();
 }
@@ -57,4 +56,33 @@ void ImportTableModelV2::setRawData(const ImportTableModelV2::raw_data_t& raw_da
     m_rawData = raw_data;
     m_maxColumnCount = maxColumnCount(m_rawData);
     endResetModel();
+}
+
+QVariant ImportTableModelV2::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal || role != Qt::DisplayRole)
+        return QVariant();
+
+    return section < utilityRowCount() ? utilityRowNames.at(section)
+                                       : QVariant(section - utilityRowCount() + 1);
+}
+
+int ImportTableModelV2::utilityRowCount() const
+{
+    return utilityRowNames.size();
+}
+
+//! Returns data from index.
+
+QVariant ImportTableModelV2::dataFromIndex(const QModelIndex& index) const
+{
+    int row = index.row() - utilityRowCount();
+    if (row >= 0 && row < static_cast<int>(m_rawData.size())) {
+        int col = index.column();
+        if (col >= 0 && col < static_cast<int>(m_rawData[row].size())) {
+            auto str = m_rawData[static_cast<int>(row)][static_cast<int>(col)];
+            return QString::fromStdString(str);
+        }
+    }
+    return QVariant();
 }
