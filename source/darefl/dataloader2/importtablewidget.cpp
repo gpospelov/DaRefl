ff8b12da6209c0cf78c18a55c2fa0ec3a43c84e3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-10-19 17:13:58 +0200

    Implement temporary delegate for ImportTableWidget

--- source/darefl/dataloader2/importtablewidget.cpp
+++ source/darefl/dataloader2/importtablewidget.cpp
@@ -12,16 +12,43 @@
 #include <QVBoxLayout>
 #include <darefl/dataloader2/importtablemodel.h>
 #include <darefl/dataloader2/importtablewidget.h>
+#include <mvvm/viewmodel/viewmodeldelegate.h>
+#include <mvvm/editors/combopropertyeditor.h>
+#include <mvvm/model/comboproperty.h>
+#include <QItemEditorFactory>
+#include <QStandardItemEditorCreator>
+#include <QStyledItemDelegate>
 
 ImportTableWidget::ImportTableWidget(QWidget* parent)
-    : QWidget(parent), m_tableModel(new ImportTableModelV2), m_tableView(new QTableView)
+    : QWidget(parent), m_tableModel(new ImportTableModelV2), m_tableView(new QTableView),
+      m_delegate(new ModelView::ViewModelDelegate), m_styledDelegate(new QStyledItemDelegate(this))
 {
     auto layout = new QVBoxLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
     layout->addWidget(m_tableView);
 
+//    m_tableView->setItemDelegate(m_delegate.get());
+
+    QItemEditorFactory *factory = new QItemEditorFactory;
+
+    QItemEditorCreatorBase *colorListCreator =
+        new QStandardItemEditorCreator<ModelView::ComboPropertyEditor>();
+
+
+    ModelView::ComboProperty combo;
+    factory->registerEditor(QVariant::fromValue(combo).userType(), colorListCreator);
+
+    m_styledDelegate->setItemEditorFactory(factory);
+    m_tableView->setItemDelegate(m_styledDelegate);
+
+
+//   m_tableView->itemDelegate()->setItemE
+
+
     m_tableView->setModel(m_tableModel);
 }
+
+ImportTableWidget::~ImportTableWidget() = default;
 
 void ImportTableWidget::setRawData(const std::vector<std::vector<std::string>>& table_data)
 {
