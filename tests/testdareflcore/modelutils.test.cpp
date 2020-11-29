6350351ed14a91d7b9cfeb17b97d917e73319f98
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-11-29 18:42:07 +0100

    Restore unit test

--- tests/testdareflcore/modelutils.test.cpp
+++ tests/testdareflcore/modelutils.test.cpp
@@ -39,32 +39,30 @@ TEST_F(ModelUtilsTest, CreateDiffVector)
 
 //! Testing SetDifference helper method.
 
-// FIXME enable test
+TEST_F(ModelUtilsTest, SetDifference)
+{
+    Data1DItem item1;
+    item1.setAxis<FixedBinAxisItem>(3, 0.0, 3.0);
+    Data1DItem item2;
+    item2.setAxis<FixedBinAxisItem>(3, 0.0, 3.0);
 
-//TEST_F(ModelUtilsTest, SetDifference)
-//{
-//    Data1DItem item1;
-//    item1.setAxis<FixedBinAxisItem>(3, 0.0, 3.0);
-//    Data1DItem item2;
-//    item2.setAxis<FixedBinAxisItem>(3, 0.0, 3.0);
+    std::vector<double> values1{1.0, 2.0, 3.0};
+    std::vector<double> values2{1.0, 4.0, -3.0};
+    item1.setValues(values1);
+    item2.setValues(values2);
 
-//    std::vector<double> values1{1.0, 2.0, 3.0};
-//    std::vector<double> values2{1.0, 4.0, -3.0};
-//    item1.setValues(values1);
-//    item2.setValues(values2);
+    Data1DItem diff;
+    diff.setAxis<FixedBinAxisItem>(3, 0.0, 3.0);
+    ::Utils::SetDifference(&item1, &item2, &diff);
 
-//    Data1DItem diff;
-//    diff.setAxis<FixedBinAxisItem>(3, 0.0, 3.0);
-//    ::Utils::SetDifference(&item1, &item2, &diff);
+    std::vector<double> expected_centers = {0.5, 1.5, 2.5};
+    EXPECT_EQ(diff.binCenters(), expected_centers);
+    auto values = diff.binValues();
 
-//    std::vector<double> expected_centers = {0.5, 1.5, 2.5};
-//    EXPECT_EQ(diff.binCenters(), expected_centers);
-//    auto values = diff.binValues();
+    EXPECT_EQ(values.size(), 3u);
 
-//    EXPECT_EQ(values.size(), 3u);
-
-//    auto diff_func = [](auto a, auto b) { return 2.0 * (a - b) / (a + b); };
-//    EXPECT_FLOAT_EQ(values[0], diff_func(values1[0], values2[0]));
-//    EXPECT_FLOAT_EQ(values[1], diff_func(values1[1], values2[1]));
-//    EXPECT_FLOAT_EQ(values[2], 0.0); // division by zero
-//}
+    auto diff_func = [](auto a, auto b) { return 2.0 * (a - b) / (a + b); };
+    EXPECT_FLOAT_EQ(values[0], diff_func(values1[0], values2[0]));
+    EXPECT_FLOAT_EQ(values[1], diff_func(values1[1], values2[1]));
+    EXPECT_FLOAT_EQ(values[2], 0.0); // division by zero
+}
