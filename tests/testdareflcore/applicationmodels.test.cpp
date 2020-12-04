649965d297cb4f8e022ae59002383bc1dea7efc5
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-12-04 18:20:51 +0100

    Test to check save/load of all models belonging to ApplicationModels

---
+++ tests/testdareflcore/applicationmodels.test.cpp
@@ -0,0 +1,52 @@
+// ************************************************************************** //
+//
+//  Reflectometry simulation software prototype
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "folderbasedtest.h"
+#include "google_test.h"
+#include <darefl/model/applicationmodels.h>
+#include <mvvm/factories/modeldocuments.h>
+#include <mvvm/project/project.h>
+#include <mvvm/project/project_types.h>
+
+using namespace ModelView;
+
+//! Tests of ApplicationModels
+
+class ApplicationModelsTest : public FolderBasedTest
+{
+public:
+    ApplicationModelsTest() : FolderBasedTest("test_ApplicationModelsTest") {}
+
+    ~ApplicationModelsTest();
+};
+
+ApplicationModelsTest::~ApplicationModelsTest() = default;
+
+//! Testing that nothing is crashing on project save and load.
+
+TEST_F(ApplicationModelsTest, saveLoad)
+{
+    auto project_dir = createEmptyDir("Untitled1");
+
+    DaRefl::ApplicationModels models, loadedModels;
+
+    {
+        ProjectContext context1;
+        context1.m_models_callback = [&models]() { return models.persistent_models(); };
+        Project project1(context1);
+        project1.save(project_dir);
+    }
+
+    {
+        ProjectContext context2;
+        context2.m_models_callback = [&loadedModels]() { return loadedModels.persistent_models(); };
+        Project project2(context2);
+        project2.load(project_dir);
+    }
+}
